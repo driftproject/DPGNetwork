1c3042ca4510cbaa9e53215d7ff6059495a12e11
Иван Кривенцев <dirtyhornet277@gmail.com>
2023-02-21 17:12:09 +0300

    Сделано рандомный выбор иконок и функция вкхода

--- player/db/player.py
+++ player/db/player.py
@@ -4,23 +4,29 @@ from security.password import PasswordHash
 from security.jwt import create_token
 from player.validation.player import RegUser, AuthUser
 from player.db.models.player import Player
+from random import randint
+from fastapi.responses import FileResponse
+from fastapi import Response
 
 engine = create_engine(env["POSTGRES_PATH"])
 SQLModel.metadata.create_all(engine)
 session = Session(engine)
+
 
 def register_user(userdata: RegUser):
     player = Player(
         username=userdata.username,
         email=userdata.email,
         hashed_password=PasswordHash.get_password_hash(userdata.password),
-        secret_key=PasswordHash.get_password_hash(userdata.secret_key)
+        secret_key=PasswordHash.get_password_hash(userdata.secret_key),
+        profile_pic_path=f"logo{randint(1,6)}.png"
     )
     session.add(player)
     session.commit()
     return_data = {
         "id": player.id,
         "username": player.username,
+        "profile_pic": player.profile_pic_path,
         "token": create_token(
             {
                 "id": player.id,
@@ -28,3 +34,38 @@ def register_user(userdata: RegUser):
             }
         )
     }
+    return return_data
+
+
+def login_user(userdata: AuthUser):
+    player = session.execute(select(Player).where(Player.username == userdata.username))
+    player = player.scalar_one_or_none()
+    if player:
+        if PasswordHash.verify_password(userdata.password, player.hashed_password):
+            return {
+                "id": player.id,
+                "username": player.username,
+                "profile_pic": player.profile_pic_path,
+                "token": create_token(
+                    {
+                        "id": player.id,
+                        "username": player.username
+                    }
+                )
+            }
+        elif PasswordHash.verify_password(userdata.password, player.secret_key):
+            return {
+                "id": player.id,
+                "username": player.username,
+                "profile_pic": player.profile_pic_path,
+                "token": create_token(
+                {
+                    "id": player.id,
+                    "username": player.username
+                }
+                )
+        }
+        else:
+            return Response("Incorrect password", 401)
+    else:
+        return Response("Incorrect password", 401)
