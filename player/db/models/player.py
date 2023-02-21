43a5d03359a3210a66cb00752b3413c34239cf87
Иван Кривенцев <dirtyhornet277@gmail.com>
2023-02-21 17:20:05 +0300

    Были ошибки в модели

--- player/db/models/player.py
+++ player/db/models/player.py
@@ -3,7 +3,7 @@ from sqlmodel import SQLModel, Field
 
 class Player(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
-    username: str = Field(max_length=20, min_length=5)
+    username: str
     email: str
     hashed_password: str
     profile_pic_path: str
