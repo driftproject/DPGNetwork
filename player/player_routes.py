b018a10abdb5328a6390f4069b966dd1a20cc4c3
Иван Кривенцев <dirtyhornet277@gmail.com>
2023-02-21 19:33:08 +0300

    Аутенфикация по токену (недействительна)

--- player/player_routes.py
+++ player/player_routes.py
@@ -1,12 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from player.validation.player import RegUser, AuthUser
-from player.db.player import register_user, login_user
+from player.db.player import register_user, login_user, get_current_user_with_jwt, login_user_with_jwt
 from fastapi.responses import FileResponse
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 _player_routing = APIRouter(
     prefix="/player"
 )
 
+oauth2 = OAuth2PasswordBearer(tokenUrl="/player/token")
 
 @_player_routing.post("/registration")
 async def _registration_user_(userdata: RegUser):
@@ -21,3 +23,11 @@ async def return_profile_pic(image: str):
 @_player_routing.post("/login")
 async def _login_user_(userdata: AuthUser):
     return login_user(userdata)
+
+@_player_routing.post("/token")
+async def _token_getter_(userdata: OAuth2PasswordRequestForm = Depends(), response_model=None):
+    return login_user_with_jwt(userdata)
+
+@_player_routing.get("/me")
+async def get_player_profile(token: str = Depends(oauth2)):
+    return get_current_user_with_jwt(token)
