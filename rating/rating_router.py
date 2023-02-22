5cf74cd24da7c1a7d916632e79065d511d82733c
Иван Кривенцев <dirtyhornet277@gmail.com>
2023-02-22 10:51:10 +0300

    Получение всех результатов

--- rating/rating_router.py
+++ rating/rating_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from rating.validation.rating import RatingSend
-from rating.db.rating import create_result, get_rating_by_userid
+from rating.db.rating import create_result, get_rating_by_userid, get_all_rating_data
 from security.oauth import oauth2
 import json
 from security.jwt import decode_token
@@ -10,10 +10,14 @@ _rating_routing = APIRouter(
 )
 
 @_rating_routing.post("/create")
-async def create_rating(rating: RatingSend, token: int = Depends(oauth2)):
-    return json.loads(create_result(rating, decode_token(token)["id"]))
+async def create_rating(rating: RatingSend, token: str = Depends(oauth2)):
+    return create_result(rating, decode_token(token)["id"])
 
 
 @_rating_routing.get("/by_user/{id}")
-async def _get_rating_by_userid(id: int, token: int = Depends(oauth2)):
+async def _get_rating_by_userid(id: int, token: str = Depends(oauth2)):
     return get_rating_by_userid(id)
+
+@_rating_routing.get("/all")
+async def _get_all_rating(token: str = Depends(oauth2)):
+    return get_all_rating_data()
