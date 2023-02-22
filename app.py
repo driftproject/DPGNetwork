e21e07fbeefaf41bb485ec35f11e0c027e8cd81f
Иван Кривенцев <dirtyhornet277@gmail.com>
2023-02-22 09:50:31 +0300

    Добавлен роутер рейтинга

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from virtual_environment import env
 from player.player_routes import _player_routing
-
+from rating.rating_router import _rating_routing
+from fastapi.security import OAuth2PasswordBearer
 
 
 app = FastAPI()
 app.include_router(
     _player_routing
+)
+
+app.include_router(
+    _rating_routing
 )
 
 app.add_middleware(
